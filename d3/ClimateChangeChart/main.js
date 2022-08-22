bee5c955a3a8a3fc56d62252608c9aaea55f42f9
Sloane Luckiewicz <sloaneluckiewicz@gmail.com>
2022-08-22 16:02:02 -0400

    highlighting last li
    highlighting ability for the last li item

--- d3/ClimateChangeChart/main.js
+++ d3/ClimateChangeChart/main.js
@@ -2,16 +2,17 @@ let climate_daly_data = [
     {region: "Low-and-middle-income countries of the African Region", deaths: 57},
     {region: "Low-and-middle-income countries of the Americas", deaths: 2},
     {region: "Low-and-middle-income countries of the Eastern Mediterranean Region", deaths: 20},
-     {region: "Low-and-middle-income countries of the European Region", deaths: 0.67},
-     {region: "Low-and-middle-income countries of the South-East Asia Region", deaths: 58},
+    {region: "Low-and-middle-income countries of the European Region", deaths: 0.67},
+    {region: "Low-and-middle-income countries of the South-East Asia Region", deaths: 58},
     {region: "Low-and-middle-income countries of the Western Pacific Region", deaths: 4},
     {region: "High income countries", deaths: 0.23}
 ];
 
 
 let toggleClass = (i,toggle) => {
-    d3.select("#viz div:nth-child("+ i +")").classed("highlightBar",toggle);
-    d3.select("#legend li:nth-child("+ i +")").classed("highlightText",toggle);
+    _i = i+1;
+    d3.select("#viz div:nth-child("+ _i +")").classed("highlightBar",toggle);
+    d3.select("#legend li:nth-child("+ _i +")").classed("highlightText",toggle);
 };
 
 let divSelection = d3.select("#viz")
@@ -22,7 +23,7 @@ divSelection
     .enter()
     .append("div")
     .attr("class", "bar")
-    .style("width", function(d) { return d.deaths * 9 + "px"})
+    .style("width", function(d) { return d.deaths * 8 + "px"})
     .on("mouseover", function(d,i) {
         toggleClass(i, true);
     })
@@ -38,7 +39,7 @@ listSelection
     .enter()
     .append("li")
     .text(function(d){
-    return `${d.region}: ${d.deaths} deaths`
+        return `${d.region}: ${d.deaths} deaths`;
     })
     .on("mouseover", function(d,i) {
         toggleClass(i, true);
